536f03cfc80fab5e9bf6c2cd69b9f7fe8301855f
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-05-30 16:52:17 +0200

    Add debug output log message when the applciation crashes

--- Editor/Program.cs
+++ Editor/Program.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
+using Avalonia.Logging;
 using Avalonia.ReactiveUI;
 
 namespace Editor
@@ -8,8 +10,17 @@ namespace Editor
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            try
+            {
+                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+            }
+            catch (Exception e)
+            {
+                Logger.Sink.Log(LogEventLevel.Fatal, "MAIN", null,e.ToString());
+            }
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
