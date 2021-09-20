8a2c7f734e96737892e1333a203621a1ac104763
Ygg01 <y.laughing.man.y@gmail.com>
2021-09-20 18:04:34 +0200

    Make saving images lazy (#25)
    * Make saving images lazy
    
    Relates to #24
    
    Images are now lazily saved i.e. not updated if they weren't touched at all.
    
    * Fix exception throwing on non-existent keys

--- Editor/Models/RSI/RsiItem.cs
+++ Editor/Models/RSI/RsiItem.cs
@@ -1,4 +1,5 @@
-﻿using Importer.RSI;
+﻿using System.Collections.Generic;
+using Importer.RSI;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 
@@ -9,12 +10,33 @@ namespace Editor.Models.RSI
         public RsiItem(Rsi? rsi = null)
         {
             Rsi = rsi ?? new Rsi();
+            foreach (var rsiState in Rsi.States)
+            {
+                _rsiUpdateStates[rsiState.Name] = UpdateState.None;
+            }
         }
 
         public Rsi Rsi { get; }
 
+        private Dictionary<string, UpdateState> _rsiUpdateStates = new();
+
         public RsiSize Size => Rsi.Size;
 
+        public void MarkAsUpdated(RsiState state, UpdateState updateState)
+        {
+            _rsiUpdateStates[state.Name] = updateState;
+        }
+        
+        public UpdateState WasModified(RsiState state)
+        {
+            if (_rsiUpdateStates.TryGetValue(state.Name, out var wasModified)) 
+                return wasModified;
+            
+            // Assume the state is new and track it
+            _rsiUpdateStates.Add(state.Name, UpdateState.Edit);
+            return UpdateState.Edit;
+        }
+        
         public void LoadImage(int index, Image<Rgba32> image)
         {
             Rsi.States[index].LoadImage(image, Size);
@@ -23,11 +45,13 @@ namespace Editor.Models.RSI
         public void AddState(RsiImage image)
         {
             Rsi.States.Add(image.State);
+            _rsiUpdateStates.Add(image.State.Name, UpdateState.Edit);
         }
 
         public void InsertState(int index, RsiImage image)
         {
             Rsi.States.Insert(index, image.State);
+            _rsiUpdateStates.TryAdd(image.State.Name, UpdateState.Edit);
         }
 
         public void RemoveState(int index)
@@ -44,5 +68,11 @@ namespace Editor.Models.RSI
                 RemoveState(index);
             }
         }
+    }
+    
+    public enum UpdateState : byte
+    {
+        None,
+        Edit
     }
 }
